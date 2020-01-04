6c2486ba095b534d652a6275538b878ee14b1db9
amyloula <mccabeamylouise@gmail.com>
2020-01-04 20:41:14 +0000

    feature(tab3) add clear all settings functionality

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -15,23 +15,32 @@ export class Tab3Page {
   permissions = [];
 
   constructor(private storageService: StorageService, private toastService: ToastService) {
-    this.setItems();
+    this.setDefaults();
     this.updateDefaultPermissions();
   }
 
   updateDefaultPermissions() {
-    this.ionItems = this.defaultItems.map((item) => {
+    this.ionItems = this.ionItems.map((item) => {
       let updated = { ...item };
       this.getPermission(item.type)
-        .then(val => {
-          updated.permissions = val;
+        .then(response => {
+          if (this.isUndefinedOrNull(response)) {
+            updated.permissions = item.permissions;
+          } else {
+            updated.permissions = response;
+          }
         });
-        return updated;
+      return updated;
     });
+    console.log(this.ionItems);
   }
 
-  setItems() {
-    this.defaultItems = this.healthkitTypes.map(({ title, type }) => {
+  isUndefinedOrNull(value) {
+    return (value === undefined || value === null);
+  }
+
+  setDefaults() {
+    this.ionItems = this.healthkitTypes.map(({ title, type }) => {
       return {
         title,
         subtitle: '',
@@ -47,11 +56,20 @@ export class Tab3Page {
 
   toggleItem(item) {
     this.storageService.updatePermision(item.type, item.permissions)
-    .then(() => this.toastService.showToast(`Changes saved to ${item.title}`));
+      .then(() => this.toastService.showToast(`Changes saved to ${item.title}`));
   }
 
   getPermission(key) {
     return this.storageService.getPermission(key);
   }
 
+  clearAllSettings() {
+    let cancelConfig = { text: 'Cancel', role: 'cancel', handler: () => console.log('ok') };
+    let confirmConfig = { side: 'bottom', icon: 'trash', handler: () => this.clearAll() };
+    this.toastService.showToastWithOptions('Are you sure you want to clear all settings?', '', 'bottom', confirmConfig, cancelConfig);
+  }
+
+  clearAll() {
+    this.storageService.clearAll().then(() => this.setDefaults());
+  }
 }
