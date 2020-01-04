b342f7c0d9e9670563dbf29105b633ce347e2fd7
amyloula <mccabeamylouise@gmail.com>
2020-01-04 19:03:33 +0000

    feature(tab3) wip permissions page with toggles for read/write

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { HKTypes } from '../interfaces/healthkit';
+import { StorageService } from '../services/storage.service';
+import { ToastService } from '../services/toast.service';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +9,49 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  defaultItems = [];
+  ionItems = [];
+  healthkitTypes = HKTypes;
+  permissions = [];
+
+  constructor(private storageService: StorageService, private toastService: ToastService) {
+    this.setItems();
+    this.updateDefaultPermissions();
+  }
+
+  updateDefaultPermissions() {
+    this.ionItems = this.defaultItems.map((item) => {
+      let updated = { ...item };
+      this.getPermission(item.type)
+        .then(val => {
+          updated.permissions = val;
+        });
+        return updated;
+    });
+  }
+
+  setItems() {
+    this.defaultItems = this.healthkitTypes.map(({ title, type }) => {
+      return {
+        title,
+        subtitle: '',
+        type,
+        value: '',
+        permissions: {
+          read: false,
+          write: false
+        }
+      };
+    });
+  }
+
+  toggleItem(item) {
+    this.storageService.updatePermision(item.type, item.permissions)
+    .then(() => this.toastService.showToast(`Changes saved to ${item.title}`));
+  }
+
+  getPermission(key) {
+    return this.storageService.getPermission(key);
+  }
 
 }
